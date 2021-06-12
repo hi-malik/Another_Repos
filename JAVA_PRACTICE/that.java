8e26b009e26d9caab64c04458bf68a1616040be0
JAIDEV SINGH MALIK <jsm@JAIDEVs-MacBook-Air.local>
2021-06-12 18:06:18 +0530

    Another_Repos

---
+++ JAVA_PRACTICE/that.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class that{
+    public static void main(String[] args)throws Exception{
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+
+        System.out.println(str);
+    }
+}
